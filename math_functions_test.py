8252b5969f523de17aab4e7fa9db9f8237d69acf
flapek <46303285+flapek@users.noreply.github.com>
2022-12-14 19:27:45 +0000

    add math methods and test file

---
+++ math_functions_test.py
@@ -0,0 +1,16 @@
+from math_functions import add
+
+
+def test_add():
+    assert add(1, 1) == 2 
+
+# def multiply(a: float, b: float) -> float:
+#     return a * b
+
+# def divide(a: float, b: float) -> float:
+#     if b == 0:
+#         raise ValueError("Parametr b cannot be null")
+#     return a / b
+
+# def subtract(a: float, b: float) -> float:
+#     return a - b
